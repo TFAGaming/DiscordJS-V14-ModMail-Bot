6087223f0576b82ce062a90de4a9a84514052dd8
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-08-15 19:51:30 +0100

    ⚙️ Configuration file changed

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  Client: {
+    TOKEN: "",
+    ID: "989650107009990698"
+  },
+
+  Handler: {
+    GUILD_ID: "949620901291368458",
+    CATEGORY_ID: "999994847047987250"
+  },
+
+  Modmail: {
+    INTERACTION_COMMAND_PERMISSIONS: ['Administrator']
+  }
+};
