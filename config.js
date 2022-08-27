3a3a8f08e83ce5971f903144275199107b8d3b3d
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-08-27 16:41:36 +0100

    ⚙️ Updated for V6.0 update

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 module.exports = {
   Client: {
-    TOKEN: "",
-    ID: ""
+    TOKEN: "", // Your bot's TOKEN. Please use this only in VISUAL STUDIO CODE, not in REPLIT. [OPTIONAL]
+    ID: "" // Your bot's ID. [REQUIRED]
   },
 
   Handler: {
-    GUILD_ID: "",
-    CATEGORY_ID: ""
+    GUILD_ID: "", // Your guild/server ID where you want the mails will be created. [REQUIRED]
+    CATEGORY_ID: "" // Custom category ID where you want the mails will be created on. You can use /setup slash command to create a category without using this variable.
   },
 
   Modmail: {
-    INTERACTION_COMMAND_PERMISSIONS: ['Administrator']
+    INTERACTION_COMMAND_PERMISSIONS: ['Administrator'], // The permissions for the slash commands that admins/server managers can access only. [REQUIRED]
+    MAIL_MANAGER_ROLES: [''] // The roles for the category where moderators can manage mails.
   }
 };
