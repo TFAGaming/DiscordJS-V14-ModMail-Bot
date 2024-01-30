df964e8cc2e4ad1291f6f9943417fe63f75fbd16
Dragon Network <102852876+Music22157031@users.noreply.github.com>
2024-01-30 14:54:30 +0530

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     client: {
         token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '' // ← Your bot ID
+        id: '1201796495196831745' // ← Your bot ID
     },
     modmail: {
-        guildId: '', // ← Your server ID
-        categoryId: '', // ← The modmail category ID
-        staffRoles: [''], // ← The modmail staff roles IDs
+        guildId: '1172018453222268981', // ← Your server ID
+        categoryId: '1201578799737868428', // ← The modmail category ID
+        staffRoles: ['1200916028507557998'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
